2f3f1ec4f34f33ae30584c11252781945b298771
romanpovol <roman.a.p@bk.ru>
2024-10-28 12:28:38 +0300

    add pytest

--- test/tests.py
+++ test/tests.py
@@ -1,23 +1,25 @@
-import unittest
-import os
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
 
 
-def test_root(test_client):
-    response = test_client.get("/healthchecker")
+def test_root(client):
+    response = client.get("/healthchecker")
     assert response.status_code == 200
     assert response.json() == {"message": "API is live"}
 
 
-def test_create_get_user(test_client):
-    response = test_client.post("/register/", json={
-        "id": 0,
-        "first_name": "Roman",
-        "last_name": "Povolotskii",
-        "password": "Password"
+def test_create_get_user(client):
+    response = client.post("/register/", json={
+      "id": 0,
+      "first_name": "Roman",
+      "last_name": "Povolotskii",
+      "password": "Password"
     })
     assert response.status_code == 201
 
-    response = test_client.get(f"/users/{response.json()['id']}")
+    response = client.get(f"/users/{response.json()['id']}")
     assert response.status_code == 200
     response_json = response.json()
     assert response_json["Status"] == "Success"
